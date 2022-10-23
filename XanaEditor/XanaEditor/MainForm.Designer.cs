2ec233964f4233631f10499ee0470f01f5a69b09
InnieSharpSoftware <116297277+InnieSharpSoftware@users.noreply.github.com>
2022-10-23 21:06:43 +0300

    first version

---
+++ XanaEditor/XanaEditor/MainForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿/*
+By InnieSharp(ix4/i#)
+*/
+namespace XanaEditor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// fastColoredTextBox1
+			// 
+			this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+		'(',
+		')',
+		'{',
+		'}',
+		'[',
+		']',
+		'\"',
+		'\"',
+		'\'',
+		'\''};
+			this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+			this.fastColoredTextBox1.BackBrush = null;
+			this.fastColoredTextBox1.BackColor = System.Drawing.Color.Black;
+			this.fastColoredTextBox1.CharHeight = 14;
+			this.fastColoredTextBox1.CharWidth = 8;
+			this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+			this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.fastColoredTextBox1.ForeColor = System.Drawing.Color.White;
+			this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.Black;
+			this.fastColoredTextBox1.IsReplaceMode = false;
+			this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.SpringGreen;
+			this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 24);
+			this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+			this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+			this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+			this.fastColoredTextBox1.Size = new System.Drawing.Size(420, 400);
+			this.fastColoredTextBox1.TabIndex = 6;
+			this.fastColoredTextBox1.Zoom = 100;
+			this.fastColoredTextBox1.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.FastColoredTextBox1TextChanged);
+			this.fastColoredTextBox1.Load += new System.EventHandler(this.FastColoredTextBox1Load);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.fileToolStripMenuItem,
+			this.runToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(420, 24);
+			this.menuStrip1.TabIndex = 4;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.openToolStripMenuItem,
+			this.saveToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItemClick);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.saveToolStripMenuItem.Text = "Save";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItemClick);
+			// 
+			// runToolStripMenuItem
+			// 
+			this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+			this.runToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+			this.runToolStripMenuItem.Text = "Docs";
+			this.runToolStripMenuItem.Click += new System.EventHandler(this.RunToolStripMenuItemClick);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "AuScript File (*.aus)|*.aus";
+			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveFileDialog1FileOk);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "AuScript File (*.aus)|*.aus";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1FileOk);
+			// 
+			// openFileDialog2
+			// 
+			this.openFileDialog2.Filter = "AuScript File (*.aus)|*.aus";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.Black;
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.richTextBox1.Location = new System.Drawing.Point(420, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(171, 424);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+			this.richTextBox1.Visible = false;
+			this.richTextBox1.WordWrap = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(591, 424);
+			this.Controls.Add(this.fastColoredTextBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.richTextBox1);
+			this.Name = "MainForm";
+			this.Text = "XanaEditor";
+			((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ XanaEditor/XanaEditor/MainForm.cs
@@ -0,0 +1,282 @@
+﻿/*
+By InnieSharp(ix4/i#)
+*/
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using FastColoredTextBoxNS;
+
+namespace XanaEditor
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Regular);
+		Style RedStyle = new TextStyle(Brushes.Red, null, FontStyle.Regular);
+		Style PurpleStyle = new TextStyle(Brushes.Purple, null, FontStyle.Regular);
+		Style BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
+		Style YellowStyle = new TextStyle(Brushes.Yellow, null, FontStyle.Regular);
+		Style PinkStyle = new TextStyle(Brushes.Pink, null, FontStyle.Regular);
+		Style astyle = new TextStyle(Brushes.BlueViolet, null, FontStyle.Regular);
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void OpenToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+		}
+		void SaveToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			saveFileDialog1.ShowDialog();
+		}
+		void RunToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if(richTextBox1.Visible == false)
+				richTextBox1.Visible = true;
+			else
+				richTextBox1.Visible = false;
+		}
+		void FastColoredTextBox1Load(object sender, EventArgs e)
+		{
+			
+		}
+		void SaveFileDialog1FileOk(object sender, CancelEventArgs e)
+		{
+			File.WriteAllLines(saveFileDialog1.FileName, fastColoredTextBox1.Lines);
+		}
+		void OpenFileDialog1FileOk(object sender, CancelEventArgs e)
+		{
+			fastColoredTextBox1.OpenFile(openFileDialog1.FileName);
+		}
+		void FastColoredTextBox1TextChanged(object sender, TextChangedEventArgs e)
+		{
+			for(int i = 0; i < fastColoredTextBox1.Lines.Count; i++)
+			{
+				if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("print "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 6, i);
+		    		rng.SetStyle(BlueStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("color "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 10, i);
+		    		rng.SetStyle(BlueStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("var "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("caption "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(BlueStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("math "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 6, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("set "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 4, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("if "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 3, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "pause")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 5, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "exit")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 4, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "clear")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 5, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("go "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 3, i);
+		    		rng.SetStyle(PurpleStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("[") && fastColoredTextBox1.Lines[i].ToLower().EndsWith("]"))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, fastColoredTextBox1.Lines[i].Length, i);
+		    		rng.SetStyle(PurpleStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("start "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 6, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("wait "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 7, i);
+		    		rng.SetStyle(BlueStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("readfile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("writefile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 10, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("crdir "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 6, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("rmdir "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 6, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("crfile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 7, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("rmfile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 7, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("movedir "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("movefile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("copyfile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("addline "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("removechars "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 12, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("substring "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 10, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("kill "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 5, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("stringlength "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 13, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("lineslength "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 12, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("exists "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 5, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "shutdown")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "restart")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 7, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower() == "sleep")
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 5, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("keyseta "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("keysetb "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("sizedir "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 8, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("sizefile "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("download "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 9, i);
+		    		rng.SetStyle(YellowStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("random "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 7, i);
+		    		rng.SetStyle(RedStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].ToLower().StartsWith("screenshot "))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, 11, i);
+		    		rng.SetStyle(PinkStyle);
+				}
+				else if(fastColoredTextBox1.Lines[i].StartsWith("//"))
+				{
+					Range rng = new Range(fastColoredTextBox1, 0, i, fastColoredTextBox1.Lines[i].Length, i);
+		    		rng.SetStyle(GreenStyle);
+				}
+			}
+			e.ChangedRange.ClearStyle(astyle);
+			
+			e.ChangedRange.SetStyle(astyle, "&&&", RegexOptions.Multiline);
+			e.ChangedRange.SetStyle(astyle, "$", RegexOptions.Multiline);
+		}
+	}
+}
